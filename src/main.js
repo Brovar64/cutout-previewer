fa7c9eba1caa07d4a61028eca6af14ebfa6741b7
Brovar64 <brovar64@gmail.com>
2025-04-07 16:03:39 +0200

    Add main process file

---
+++ src/main.js
@@ -0,0 +1,115 @@
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const path = require('path');
+const fs = require('fs');
+const Store = require('electron-store');
+
+const store = new Store();
+
+let controlWindow = null;
+let previewWindow = null;
+let cutouts = [];
+
+function createControlWindow() {
+  controlWindow = new BrowserWindow({
+    width: 400,
+    height: 600,
+    title: 'Cutout Previewer',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    backgroundColor: '#222222'
+  });
+
+  controlWindow.loadFile(path.join(__dirname, 'control.html'));
+  
+  controlWindow.on('closed', () => {
+    app.quit();
+  });
+}
+
+function createPreviewWindow() {
+  previewWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  previewWindow.loadFile(path.join(__dirname, 'preview.html'));
+  
+  previewWindow.setIgnoreMouseEvents(false);
+}
+
+app.whenReady().then(() => {
+  createControlWindow();
+  createPreviewWindow();
+  
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createControlWindow();
+      createPreviewWindow();
+    }
+  });
+  
+  const lastFolder = store.get('lastFolder');
+  if (lastFolder) {
+    scanFolder(lastFolder);
+  }
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+ipcMain.on('select-folder', async () => {
+  const result = await dialog.showOpenDialog(controlWindow, {
+    properties: ['openDirectory']
+  });
+  
+  if (!result.canceled) {
+    const folderPath = result.filePaths[0];
+    store.set('lastFolder', folderPath);
+    scanFolder(folderPath);
+  }
+});
+
+function scanFolder(folderPath) {
+  fs.readdir(folderPath, (err, files) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    
+    const pngFiles = files.filter(file => file.toLowerCase().endsWith('.png'));
+    cutouts = pngFiles.map(file => {
+      return {
+        name: file,
+        path: path.join(folderPath, file)
+      };
+    });
+    
+    controlWindow.webContents.send('cutouts-updated', cutouts);
+  });
+}
+
+ipcMain.on('add-cutout', (event, cutoutPath) => {
+  previewWindow.webContents.send('add-cutout', cutoutPath);
+});
+
+ipcMain.on('update-cursor', (event, isOverDraggable) => {
+  if (!previewWindow) return;
+  
+  if (isOverDraggable) {
+    previewWindow.setIgnoreMouseEvents(false);
+  } else {
+    previewWindow.setIgnoreMouseEvents(false, { forward: true });
+  }
+});
